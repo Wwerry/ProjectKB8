64291a2e6dcad0dbaf8017a719af715b33f3c268
Arina <arinabokareva10@yandex.ru>
2026-02-20 21:06:01 +0300

    Добавлен код

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.CompilerServices;
+
+public class Program
+{
+    public static void Main()
+    {
+
+
+    }
+}
+
+class Department 
+{
+    public List<Employee> Employees { get; set; } = new List<Employee>();
+    public void addEmployee(Employee employee) 
+    {
+    }
+    public void deleteEmployee(Employee employee) { }
+    public void displayInfo() { }
+
+
+}
+abstract class Employee
+{
+    public int Age { get; set; }
+    public string Name { get; set; }
+    public int Salary { get; set; }
+
+    public abstract void PerformDuties();
+}
+class Manager : Employee
+{
+    public int KPI { get; set; }
+    public string Project { get; set; }
+    public override void PerformDuties() { }
+}
+
+class Developer : Employee
+{
+    public string Stack { get; set; }
+    public string Skill { get; set; }
+    public override void PerformDuties() { }
+}
+
