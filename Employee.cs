4696c9d58f0a3ea3d060495117f011d4c9f50c20
Bogdan V <bogdan_lavrinyuk@mail.ru>
2026-02-22 23:26:15 +0300

    Удалил дубликаты файлов из корня

--- Employee.cs
+++
@@ -1,8 +0,0 @@
-abstract class Employee
-{
-    public int Age { get; set; }
-    public string Name { get; set; }
-    public int Salary { get; set; }
-
-    public abstract void PerformDuties();
-}
--- Manager.cs
+++
@@ -1,6 +0,0 @@
-class Manager : Employee
-{
-    public int KPI { get; set; }
-    public string Project { get; set; }
-    public override void PerformDuties() { }
-}
